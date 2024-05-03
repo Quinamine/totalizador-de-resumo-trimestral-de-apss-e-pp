d1a81152ad65efa73fa85227b0e822b47bedb595
Adérito Félix Quinamine <dery.felix.df@gmail.com>
2024-05-03 08:33:05 -0300

    Criei as funcionalidades do menu (Realçar totais, Ir para linha, Esvaziar ficha, Imprimir e Abrir artigos 'Sobre' e 'Ajuda'). Criei um database local para sugestão de Província, Distrito, US e Ano nos inputs específicos.

---
+++ fisiologia/menu.js
@@ -0,0 +1,270 @@
+"use strict"
+const menu = {
+    realcarTotaisSe(condicao) {
+        const totais = document.querySelectorAll("[readonly]");
+        for (const t of totais) {
+            if(condicao) {
+                t.classList.add("--realcar-totais");
+                localStorage.setItem(`${keyPrefix}-realcarTotais`, "true");
+            } else {
+                t.classList.remove("--realcar-totais");
+                localStorage.removeItem(`${keyPrefix}-realcarTotais`);
+            }
+        }
+    },
+
+    irParaLinha() {
+        return {
+            dialogBox: document.querySelector(".dialog-box-ir-para"),
+            inputNumLinha: document.querySelector(".dialog-box-ir-para__input-linha"),
+            numerosDeLinha: document.querySelectorAll(".ficha__col-de-nums-de-linhas span"),
+
+            abrirDialogBox() { 
+                menu.irParaLinha().dialogBox.classList.add("--open");
+                menu.irParaLinha().inputNumLinha.value = "";
+                menu.irParaLinha().inputNumLinha.focus();
+            },
+
+            fecharDialogBox() {
+                menu.irParaLinha().dialogBox.classList.remove("--open");
+                menu.irParaLinha().removeLnHighlight();
+            },
+
+            goToLn(numLinha) {
+                this.removeLnHighlight(); 
+                numLinha = formatarString(numLinha)
+                let nL = this.numerosDeLinha;
+
+                let numLinhaMatches = false;
+                for(let i = 0; i < nL.length; i++) {
+                    if(formatarString(nL[i].textContent) === numLinha) {
+                            numLinhaMatches = true;
+                            let newIndex = i;
+                            if(window.innerWidth > 998) newIndex -= 2;
+                            i > 2 ? nL[newIndex].parentElement.scrollIntoView() : document.body.scrollIntoView(); 
+                            this.highlightLnFound(nL[i].parentElement);        
+                    }
+                }  
+              
+                if(!numLinhaMatches) {
+                    const msg = "Nenhuma linha corresponde à referência digitada.";
+                    alertarSobre(msg);
+                }
+            },
+
+            highlightLnFound(lnFound) {
+                this.removeLnHighlight();
+                lnFound.classList.add("ficha__num-de-linha", "--highlight");
+            },
+
+            removeLnHighlight() {
+                for(const num of this.numerosDeLinha) {
+                    num.parentElement.classList.remove("--highlight");
+                }
+            }
+        }
+    },
+
+    esvaziarFicha() {
+        return {  
+            dialogBox: document.querySelector(".dialog-box-esvaziar-ficha"),
+            abrirDialogBox() { 
+                const gridInputs  = document.querySelectorAll("[data-totalgeral], [readonly], .grid-extra__input");
+
+                let inputFilled = 0;
+                for(const input of gridInputs) {
+                    input.value.length > 0 && inputFilled++;
+                }
+
+                if(inputFilled === 0) {
+                    const noInputFilledMsg = "A ficha encontra-se vazia actualmente."
+                    alertarSobre(noInputFilledMsg);
+                    return false;
+                } 
+
+                menu.esvaziarFicha().dialogBox.classList.add("--open");
+                desfoqueDoFundo("desfocar");
+            },
+
+            fecharDialogBox() {
+                menu.esvaziarFicha().dialogBox.classList.remove("--open");
+                desfoqueDoFundo("focar");
+                removerDestaqueDeRedCells();
+            },
+
+            confirmar() {
+                const gridInputs  = document.querySelectorAll("[data-totalgeral], [readonly], .grid-extra__input");
+                const dadosAdicionais__checkboxes = document.querySelectorAll("[data-inputadicionalid]");
+       
+                for (let i = 0; i < gridInputs.length; i++) {
+                    gridInputs[i].value = "";
+                    localStorage.removeItem(`${keyPrefix}-input${i}`);
+                }
+
+                for (const cb of dadosAdicionais__checkboxes) {                    
+                    if(cb.checked) {
+                        let id = cb.dataset.inputadicionalid;
+                        let inputAdicional = document.getElementById(`${id}`);
+                        inputAdicional.value = "";
+                        localStorage.removeItem(`${keyPrefix}-${inputAdicional.id}`);
+                    }
+                }
+                menu.esvaziarFicha().fecharDialogBox();
+            }
+        }
+    },
+
+    imprimirFicha() {
+        const comentarios = document.querySelector(".main__campo-de-nota");
+        comentarios.value === "" && comentarios.classList.add("--no-print");
+        window.print()
+    },
+
+    abrirArtigo(artigo) {
+        const artigoSobre = document.querySelector(".artigo--sobre");
+        const artigoAjuda = document.querySelector(".artigo--ajuda");
+        const body = document.querySelector(".body");
+
+        artigo === "sobre" ? 
+        artigoSobre.classList.add("--open") : 
+        artigoAjuda.classList.add("--open");
+
+        body.classList.add("--overflow-h");
+        desfoqueDoFundo("desfocar");
+    },
+
+    fecharArtigo(artigo) {
+        const artigoSobre = document.querySelector(".artigo--sobre");
+        const artigoAjuda = document.querySelector(".artigo--ajuda");
+        const body = document.querySelector(".body");
+
+        artigo === "sobre" && artigoSobre.classList.remove("--open");
+
+        if(artigo === "ajuda") {
+            const details = document.getElementsByTagName("details");
+            for (const d of details) {
+                d.removeAttribute("open");
+            }
+            artigoAjuda.classList.remove("--open");
+        }
+
+        body.classList.remove("--overflow-h");
+        desfoqueDoFundo("focar");
+    }
+}
+
+function eventos() {
+    // REALCAR TOTAIS
+    const checkboxRealcarTotais = document.getElementById("checkbox-realcar-totais");
+    const cRt = checkboxRealcarTotais;
+    cRt.addEventListener("change", () => cRt.checked ? menu.realcarTotaisSe(1) : menu.realcarTotaisSe(0));
+
+    // Realcar totais no load do windows 
+    if(localStorage.getItem(`${keyPrefix}-realcarTotais`)) {
+        checkboxRealcarTotais.setAttribute("checked", "checked");
+        menu.realcarTotaisSe(1);
+    }
+
+    // IR PARA LINHA
+    const btnAbrirIrPara = document.querySelector(".header__nav__btn--ir-para");
+    btnAbrirIrPara.addEventListener("click", menu.irParaLinha().abrirDialogBox);
+
+    const btnFecharIrPara = document.querySelector(".dialog-box-ir-para__btn-fechar");
+    btnFecharIrPara.addEventListener("click", menu.irParaLinha().fecharDialogBox);
+
+    const irPara__btnIr = document.querySelector(".dialog-box-ir-para__btn-ir");
+    irPara__btnIr.addEventListener("click", () => { 
+        const inputNumLinha = document.querySelector(".dialog-box-ir-para__input-linha");
+        if(inputNumLinha.value === "") {
+            alertarSobre("Por favor, preencha o campo com a referência da linha para a qual deseja rolar.");
+            inputNumLinha.focus();
+        } else {menu.irParaLinha().goToLn(inputNumLinha.value);}
+         
+    });
+
+    // Fechar dialog-boxes-default
+    const btnsFecharDialogBox = document.querySelectorAll(".dialog-box-default__btn");
+    btnsFecharDialogBox.forEach( btn => {
+        btn.addEventListener("click", () => {
+            let btnParent = btn.parentElement;
+            btnParent.parentElement.classList.remove("--open");
+            clearInterval(btnAutoCloseLoop);
+        });
+    });
+
+    // ESVAZIAR FICHA 
+    const btnEsvaziarFicha = document.querySelector(".header__nav__btn--esvaziar-ficha");
+    btnEsvaziarFicha.addEventListener("click", menu.esvaziarFicha().abrirDialogBox);
+
+    const btnCancelar = document.querySelector(".dialog-box-esvaziar-ficha__btn--cancelar");
+    btnCancelar.addEventListener("click", menu.esvaziarFicha().fecharDialogBox);
+
+    const btnConfirmar = document.querySelector(".dialog-box-esvaziar-ficha__btn--confirmar");
+    btnConfirmar.addEventListener("click", menu.esvaziarFicha().confirmar);
+
+    // IMPRIMIR 
+    const btnImprimir = document.querySelector(".header__nav__btn--imprimir");
+    btnImprimir.addEventListener("click", menu.imprimirFicha);
+
+    // Artigos
+    const btnAbrirSobre = document.querySelector(".header__nav__btn--sobre");
+    btnAbrirSobre.addEventListener("click", () => menu.abrirArtigo("sobre"));
+
+    const btnFecharSobre = document.querySelector(".artigo__btn-fechar--sobre")
+    btnFecharSobre.addEventListener("click", () => menu.fecharArtigo("sobre"));
+
+    window.addEventListener("resize", () => {
+        const artigoSobre = document.querySelector(".artigo--sobre");
+
+        const itsMobile = window.innerWidth < 1024;
+        const articleIsOpen = artigoSobre.matches(".--open");
+        const body = document.querySelector(".body");
+
+        if(itsMobile && articleIsOpen) {
+            desfoqueDoFundo("focar");
+            location.href = `index.html#${artigoSobre.id}`;
+            body.classList.remove("--overflow-h");
+            
+        } else if(!itsMobile && articleIsOpen) {
+            desfoqueDoFundo("desfocar");
+            body.classList.add("--overflow-h");
+        }       
+    });
+
+    const btnAbrirAjuda = document.querySelector(".header__nav__btn--ajuda");
+    btnAbrirAjuda.addEventListener("click", () => menu.abrirArtigo("ajuda"));
+
+    const btnFecharAjuda = document.querySelector(".artigo__btn-fechar--ajuda")
+    btnFecharAjuda.addEventListener("click", () => menu.fecharArtigo("ajuda"));
+
+    // PARTILHAR 
+    const data = {
+        title: "Totalizador de Resumo Mensal de HIV/SIDA",
+        text: "O Totalizador de Resumo Mensal de HIV/SIDA é um serviço online gratuito que auxilia na elaboração, como o nome sugere, do resumo mensal de HIV/SIDA, por meio do cálculo automático dos totais com base nos dados preenchidos pelo usuário. Foi criado de acordo com o modelo da ficha de resumo mensal de HIV/SIDA actualmente vigente no Serviço Nacional de Saúde em Moçambique.",
+        url: "https://quinamine.github.io/totalizador-de-resumo-mensal-de-hiv-e-sida/index.html"
+    }
+
+    const btnPartilhar = document.querySelector(".header__nav__btn--partilhar");
+    btnPartilhar.addEventListener("click", () => {
+        try {
+            navigator.share(data).then(()=>console.log("Totalizador partilhado com sucesso."))
+            .catch(e=> console.log(`Não foi possivel partilhar o totalizador devido ao erro: ${e}.`))
+        } catch (e) {
+            console.log("O seu navegador não tem suporte ao método 'navigator.share()'.")
+        }
+    })
+
+};
+
+window.addEventListener("load", eventos);
+
+window.addEventListener("keydown", event => {
+
+    // CONTROL = 17 && p = 80
+    if(event.ctrlKey && event.keyCode === 80) {
+        event.preventDefault();
+        menu.imprimirFicha();
+    }
+})
+
+
---
+++ fisiologia/multifuncoes.js
@@ -0,0 +1,134 @@
+var keyPrefix = "trmapss";
+
+function desfoqueDoFundo(accao) {
+    const desfoque = document.querySelector(".desfoque");
+    accao === "desfocar" ? 
+    desfoque.classList.add("on") :
+    desfoque.classList.remove("on");
+}
+
+function alertarSobre(msg) {
+    const dialogBoxDefault = document.querySelector(".dialog-box-default--small");
+    const dialogBoxDefault__console = dialogBoxDefault.querySelector(".dialog-box-default__p--js-console");
+
+    dialogBoxDefault__console.textContent = msg;
+
+    clearInterval(btnAutoCloseLoop);
+    let time = 15;
+    const btn__outputTime = document.querySelector(".dialog-box-default__output-autoclose-loop");
+    btn__outputTime.textContent = `(${time--}s)`;
+    btnAutoCloseLoop = setInterval(() => {
+        btn__outputTime.textContent = `(${time--}s)`;
+        if(time < 0) {
+            dialogBoxDefault.classList.remove("--open");
+            clearInterval(btnAutoCloseLoop);
+        }
+
+    }, 1000);
+    dialogBoxDefault.classList.add("--open");
+}
+
+function destacarCelulasSaturadas() {
+    const celulas = document.querySelectorAll("[data-totalgeral], [readonly]");
+
+    let celulasSaturadas = 0;
+    for(const c of celulas) {
+        c.classList.remove("celula-saturada");
+        if(c.value.length > 7) {
+            c.classList.add("celula-saturada");
+            celulasSaturadas++;
+        }
+    }
+    
+    if(celulasSaturadas > 0) {
+        setTimeout(() => {
+            motivoDeSaturacao = document.querySelector(".artigo__details-motivo-de-red-cells");
+
+            menu.abrirArtigo("ajuda");
+            motivoDeSaturacao.setAttribute("open", "");
+            motivoDeSaturacao.scrollIntoView();
+        }, 2500);
+    }  
+}
+
+function removerDestaqueDeRedCells() {
+    const celulas = document.querySelectorAll("[data-totalgeral], [readonly]");
+
+    for (const c of celulas) c.classList.remove("celula-saturada");
+}
+
+const aqd = {
+    mostrarAviso() {
+        if(!sessionStorage.getItem(`${keyPrefix}-aviso-aqd`)) {
+            const avisoDeAQD = document.querySelector(".dialog-box-default--sobre-aqd");
+            setTimeout(() => avisoDeAQD.classList.add("--open"), 3000);
+        }
+    },
+
+    salvarCiencia() {
+        sessionStorage.setItem(`${keyPrefix}-aviso-aqd`, `user:aware`);
+    }
+}
+
+function actualizarAno() {
+    const tempo = new Date();
+    anoActual = tempo.getFullYear();
+
+    if(anoActual < 2023) anoActual = 2023;
+
+    const currentYearOutput = document.querySelector(".footer__current-year");
+    currentYearOutput.textContent = anoActual;
+}
+
+function formatarNumeros() {
+    const numeros = document.querySelectorAll(".number-format");
+
+    for (const n of numeros) {
+        n.textContent = Number(n.textContent).toLocaleString();
+    }
+}
+
+function animarJanelaAberta(event) {
+    const janela = document.querySelector(".dialog-box-esvaziar-ficha");
+    if(janela.matches(".--open")) {
+        event === "mousedown" ? 
+        janela.classList.add("--mexer") : 
+        janela.classList.remove("--mexer");
+    }
+}
+
+let btnAutoCloseLoop;
+window.addEventListener("load", () => {
+    const readonlyInputs = document.querySelectorAll("[readonly]");
+    readonlyInputs.forEach ( inputTarget => inputTarget.addEventListener("click", () => {
+        const readonlyInputsMsg = "Os totais estão inacessíveis para assegurar que não sejam modificados.";
+        alertarSobre(readonlyInputsMsg);
+    }));
+
+    const gridInputs = document.querySelectorAll("[data-totalgeral]");
+    gridInputs.forEach (gi => gi.addEventListener("input", destacarCelulasSaturadas));
+    destacarCelulasSaturadas();
+
+    // Indicador nao aplicavel para APEs
+    const celulasNaoAplicaveis = document.querySelectorAll(".celula-n-a");  
+    celulasNaoAplicaveis.forEach( celula => {
+        celula.addEventListener("click", () => {
+            const msg = `O indicador não se aplica aos pacientes de ${celula.dataset.faixaetaria}.`;
+            alertarSobre(msg);
+        })
+    })
+
+    
+    aqd.mostrarAviso();
+    const dialogBoxAQD__btn = document.querySelector(".dialog-box-default__btn--aqd");
+    dialogBoxAQD__btn.addEventListener("click", aqd.salvarCiencia);
+
+    // Actualizar o ano 
+    actualizarAno()
+    formatarNumeros();
+
+    // Animar Janela Aberta
+    const desfoque = document.querySelector(".desfoque");
+    desfoque.addEventListener("mousedown", event => animarJanelaAberta(event.type));
+    desfoque.addEventListener("mouseup", event => animarJanelaAberta(event.type));
+});
