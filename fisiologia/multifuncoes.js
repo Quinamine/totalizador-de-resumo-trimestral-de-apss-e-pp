ab475cd9588b75bf64419a502490f8273d57674b
Adérito Félix Quinamine <dery.felix.df@gmail.com>
2024-05-30 01:36:20 -0300

    HTML: Envolvi as datas com a tag time. CSS: Adicionei background: greenyellow no :focus das células. JS: renomeei algumas funções para mais legibilidade (destacarCelulasComConteudoOmisso e actualizarAnoDeCopyright).

--- fisiologia/multifuncoes.js
+++ fisiologia/multifuncoes.js
@@ -30,7 +30,7 @@ function alertarSobre(msg) {
     dialogBoxDefault.classList.add("--open");
 }
 
-function destacarCelulasSaturadas() {
+function destacarCelulasComConteudoOmisso() {
     const celulas = document.querySelectorAll("[data-totalgeral], [readonly]");
 
     let celulasSaturadas = 0;
@@ -72,7 +72,7 @@ const aqd = {
     }
 }
 
-function actualizarAno() {
+function actualizarAnoDeCopyright() {
     const tempo = new Date();
     let anoActual = tempo.getFullYear();
 
@@ -108,8 +108,8 @@ window.addEventListener("load", () => {
     }));
 
     const inputsCelulares = document.querySelectorAll("[data-totalgeral]");
-    inputsCelulares.forEach (inputCelular => inputCelular.addEventListener("input", destacarCelulasSaturadas));
-    destacarCelulasSaturadas();
+    inputsCelulares.forEach (inputCelular => inputCelular.addEventListener("input", destacarCelulasComConteudoOmisso));
+    destacarCelulasComConteudoOmisso();
 
     // Indicador nao aplicavel para APEs
     const celulasNaoAplicaveis = document.querySelectorAll(".celula-n-a");  
@@ -126,7 +126,7 @@ window.addEventListener("load", () => {
     dialogBoxAQD__btn.addEventListener("click", aqd.salvarCiencia);
 
     // Actualizar o ano 
-    actualizarAno()
+    actualizarAnoDeCopyright()
     formatarNumeros();
 
     // Animar Janela Aberta
